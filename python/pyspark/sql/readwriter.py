954636a2ec587a51785054987c24a595588f8d2b
aandrade <aandrade@palantir.com>
2018-11-19 15:35:14 +0100

    reverted change x2

--- python/pyspark/sql/readwriter.py
+++ python/pyspark/sql/readwriter.py
@@ -606,6 +606,7 @@ class DataFrameWriter(OptionUtils):
         """Specifies the underlying output data source.
 
         :param source: string, name of the data source, e.g. 'json', 'parquet'.
+        
         >>> df.write.format('json').save(os.path.join(tempfile.mkdtemp(), 'data'))
         """
         self._jwrite = self._jwrite.format(source)
